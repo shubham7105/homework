8d33c68f27a2f2bdd0d50f4acc811291be290e99
shubhamentelki <sbhutkar1@gmail.com>
2019-07-05 08:55:58 +0530

    circle program added to git

---
+++ learning/src/learning/Circle.java
@@ -0,0 +1,26 @@
+package learning;
+
+public class Circle {
+
+	public Circle(int i, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/*public int calRadius(int ,String ) {
+		int r= Math.PI*i*i;
+		return r;
+	}*/
+
+
+
+	public int getRadius() {
+		// TODO Auto-generated method stub
+		return 2;
+	}
+
+
+	public String getColor() {
+		// TODO Auto-generated method stub
+		return "red";
+	}}
